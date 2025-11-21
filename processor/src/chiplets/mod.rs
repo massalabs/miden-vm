b7aabd0c902ce84c66afe02415176c0cd87862e9
Leo-Besancon <lb@massa.net>
2025-11-21 11:18:43 +0100

    chore: update visibility for aux trace generation

--- processor/src/chiplets/mod.rs
+++ processor/src/chiplets/mod.rs
@@ -21,9 +21,9 @@ use memory::Memory;
 pub use memory::MemoryError;
 
 mod ace;
-use ace::AceHints;
 pub use ace::{
-    Ace, CircuitEvaluation, MAX_NUM_ACE_WIRES, PTR_OFFSET_ELEM, PTR_OFFSET_WORD, eval_circuit,
+    Ace, AceHints, CircuitEvaluation, MAX_NUM_ACE_WIRES, PTR_OFFSET_ELEM, PTR_OFFSET_WORD,
+    eval_circuit,
 };
 
 mod kernel_rom;
@@ -31,7 +31,7 @@ use kernel_rom::KernelRom;
 
 mod aux_trace;
 
-pub(crate) use aux_trace::AuxTraceBuilder;
+pub use aux_trace::AuxTraceBuilder;
 
 #[cfg(test)]
 mod tests;
--- processor/src/lib.rs
+++ processor/src/lib.rs
@@ -71,7 +71,7 @@ pub use host::{
 
 mod chiplets;
 use chiplets::Chiplets;
-pub use chiplets::MemoryError;
+pub use chiplets::{AceHints, AuxTraceBuilder, MemoryError};
 
 mod trace;
 use trace::TraceFragment;
